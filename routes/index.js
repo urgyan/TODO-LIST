a4848b864dfaa80d404e9fed3dd4da318c0d4bc8
urgyan <urgyanue195125ece@gmail.com>
2021-11-01 17:34:24 +0530

    added controller

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 //use express router
 const router = express.Router();
 console.log("router is loading!!");
-
-
+const homeController = require('../controllers/home_controller')
+router.get('/',homeController.home);
 
 
 module.exports = router;
