7cfed63d1086ee1a616a470e4b0657b56917481b
urgyan <urgyanue195125ece@gmail.com>
2021-11-01 17:06:05 +0530

    my adding empty  files and server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const port = 9000;
+
+const app = express();
+
+
+
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`There is an error on port number :- ${port}`);
+        return;
+    }
+
+    console.log(`Server is up and running on port number ${port}`);
+
+});
